b77641b257b038883db31991ca3af8352bad32cf
neyneyalldayday <drfrankenstein123@gmail.com>
2022-11-13 22:16:26 -0600

    i really dont know man lol

--- assets/js/company.js
+++ assets/js/company.js
@@ -1,56 +1,40 @@
-let m14 = document.querySelector(".m14")
-let letters = doucment.querySelector(".letters")
+let company = document.querySelector(".company")
+let letters = document.querySelector(".letters")
+let warning;
 
 
 
- ml4 = {};
-ml4.opacityIn = [0,1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
-ml4.delay = 500;
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml4 .letters-1',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-1',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4',
-    opacity: 0,
-    duration: 500,
-    delay: 500
-  });
+
+function displayWarning(){
+  // for (let i =0; i < letters.length; i++){
+   
+  // }
+   warning = setInterval(function (event) { 
+    let warn = document.createElement('p')
+    warn.className = warn;
+    warn.innerHTML += "dont"
+    warn.innerHTML += "ask"
+    warn.innerHTML += "you"
+    warn.innerHTML += "will"
+    warn.innerHTML += "die"
+    letters.append(warn)
+  }, 100)
+
+   
+
+  
+}
+
+company.addEventListener("mouseover", (event) => {   
+    displayWarning()
+})
+
+company.addEventListener("mouseout", (event) => {   
+    clearInterval(warning)
+})
+
+
+
+
+
